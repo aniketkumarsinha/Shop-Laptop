1ddcd660252885caea0b5a68804438b22531c50a
Aniket Kumar Sinha <sinhaaniket32@gmail.com>
2020-08-09 13:00:54 +0530

    Updates

---
+++ precache-manifest.48540c677c7729af1e0eac5b89410603.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c954806f530963ad1fda878636c1c4",
+    "url": "/Shop-Laptop/index.html"
+  },
+  {
+    "revision": "149c9bd4d8e7d0ab312b",
+    "url": "/Shop-Laptop/static/css/2.6417943d.chunk.css"
+  },
+  {
+    "revision": "884c8843ba0d3e8acaee",
+    "url": "/Shop-Laptop/static/css/main.493b7840.chunk.css"
+  },
+  {
+    "revision": "149c9bd4d8e7d0ab312b",
+    "url": "/Shop-Laptop/static/js/2.6571f6b0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Shop-Laptop/static/js/2.6571f6b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "884c8843ba0d3e8acaee",
+    "url": "/Shop-Laptop/static/js/main.20121b6f.chunk.js"
+  },
+  {
+    "revision": "bb71ef43f07a3235c6c7",
+    "url": "/Shop-Laptop/static/js/runtime-main.ec4ee05d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Shop-Laptop/static/media/logo.5d5d9eef.svg"
+  }
+]);
